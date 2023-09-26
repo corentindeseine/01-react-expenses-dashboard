9e430a42f4b5bc171067c7b2e6e62408c8acfb89
CorentinDeseine <deseinecorentin@gmail.com>
2023-09-27 00:34:53 +0200

    wip

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>September 26th 2023</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>132.5 €</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
