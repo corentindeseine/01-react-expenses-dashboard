7c4e03ffcd67034585feed19a6487a6521e39be3
CorentinDeseine <deseinecorentin@gmail.com>
2023-10-02 23:27:37 +0200

    fix issues

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     {
       id: 'e3',
       title: 'Car Insurance',
-      amount: 294.67,
+      amount: 294.67, 
       date: new Date(2021, 2, 28),
     },
     {
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,7 @@ import "./Card.css"
 
 function Card(props) {
   const classes = 'card ' + props.className
+  console.log(props.children)
 
   return <div className={classes}>{props.children}</div>;
 }
