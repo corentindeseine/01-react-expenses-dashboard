05805bec2ed48e478ea482540f78cafc5dcd45e8
CorentinDeseine <deseinecorentin@gmail.com>
2023-10-10 16:35:03 +0200

    session 78 : add event submit on form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -30,30 +30,43 @@ const ExpenseForm = () => {
     } else if (identifier === 'amount') {
       setEnteredAmount(value);
     } else  {
-      setEnteredTitle(value);
+      setEnteredDate(value);
     }
   };
 
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-  return <form>
-    <div className='new-expense__controls'>
-      <div className='new-expense__control'>
-        <label>Title</label>
-        <input type='text' onChange={ (event) => inputChangeHandler('title', event.target.value) } />
+    const expensedData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: enteredDate,
+    }
+    console.log(expensedData)
+  }
+
+
+  return(
+    <form onSubmit={submitHandler} >
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label>Title</label>
+          <input type='text' onChange={ (event) => inputChangeHandler('title', event.target.value) } />
+        </div>
+        <div className='new-expense__control'>
+          <label>Amount</label>
+          <input type='text' min="0.0.1" step="0.0.1" onChange={ (event) => inputChangeHandler('amount', event.target.value) } />
+        </div>
+        <div className='new-expense__control'>
+          <label>Date</label>
+          <input type='date' min="2020-01-01" max="2023-12-31" onChange={ (event) => inputChangeHandler('date', event.target.value) } />
+        </div>
       </div>
-      <div className='new-expense__control'>
-        <label>Amount</label>
-        <input type='text' min="0.0.1" step="0.0.1" onChange={ (event) => inputChangeHandler('amount', event.target.value) } />
+      <div className="new-expense__actions">
+        <button type='submit'>Add Expense</button>
       </div>
-      <div className='new-expense__control'>
-        <label>Date</label>
-        <input type='date' min="2020-01-01" max="2023-12-31" onChange={ (event) => inputChangeHandler('date', event.target.value) } />
-      </div>
-    </div>
-    <div className="new-expense__actions">
-      <button type='submit'>Add Expense</button>
-    </div>
-  </form>
+    </form>
+  )
 };
 
 export default ExpenseForm;
