446e1f016d926dabb8131e22bb26b3b406816807
CorentinDeseine <deseinecorentin@gmail.com>
2023-10-10 16:28:05 +0200

    refacto multiple changeHandler function in 1 uniq function

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
+
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  /// Si on veut utiliser onChange={$NAME$ChangeHandler} ///
+
+  // const titleChangeHandler = (event) => {
+  //   setEnteredTitle(event.target.value);
+  // }
+
+  // const amountChangeHandler = (event) => {
+  //   setEnteredAmount(event.target.value);
+  // }
+
+  // const dateChangeHandler = (event) => {
+  //   setEnteredDate(event.target.value);
+  // }
+
+  /// Une seule fonction refacto prenant en compte 2 params ///
+
+  const inputChangeHandler = (identifier, value) => {
+    if (identifier ==='title') {
+      setEnteredTitle(value);
+      console.log(value)
+    } else if (identifier === 'amount') {
+      setEnteredAmount(value);
+    } else  {
+      setEnteredTitle(value);
+    }
+  };
+
+
   return <form>
     <div className='new-expense__controls'>
       <div className='new-expense__control'>
         <label>Title</label>
-        <input type='text'/>
+        <input type='text' onChange={ (event) => inputChangeHandler('title', event.target.value) } />
       </div>
       <div className='new-expense__control'>
         <label>Amount</label>
-        <input type='text' min="0.0.1" step="0.0.1"/>
+        <input type='text' min="0.0.1" step="0.0.1" onChange={ (event) => inputChangeHandler('amount', event.target.value) } />
       </div>
       <div className='new-expense__control'>
         <label>Date</label>
-        <input type='date' min="2020-01-01" max="2023-12-31"/>
+        <input type='date' min="2020-01-01" max="2023-12-31" onChange={ (event) => inputChangeHandler('date', event.target.value) } />
       </div>
     </div>
     <div className="new-expense__actions">
